6ff21d0351f93f4a52eceaaa617f9970f7a5fdec
sawamotokai <kaisawamoto@gmail.com>
2022-07-07 23:47:59 -0700

    feat: parse problems concurrently

--- parser.py
+++ parser.py
@@ -5,6 +5,8 @@ import urllib3
 import subprocess
 import os
 import sys
+import concurrent.futures
+import urllib.request
 from os.path import expanduser
 from colorama import init
 from colorama import Fore, Back, Style
@@ -12,10 +14,12 @@ from colorama import Fore, Back, Style
 from utils import *
 from config import *
 
+
 #TODO organize import
 
 home = expanduser("~")
 PWD = os.getcwd()
+http = urllib3.PoolManager()
 
 class ParserFactory(object):
     @staticmethod
@@ -26,7 +30,6 @@ class ParserFactory(object):
             return CodeForcesParser()
         else:
             raise Exception("Ilegal Input")
-
 
 class Parser(object):
     def __init__(self):
@@ -43,87 +46,78 @@ class AtCoderParser(Parser):
 class CodeForcesParser(Parser):
     def  __init__(self):
         super().__init__()
+        self.code = 0
+
+    def parseProblem(self, c):
+        url = f"{codeForcesURL}{str(self.code)}/problem/{str(c)}"
+        page = http.request("Get", url)
+        soup = BeautifulSoup(page.data, features="lxml")
+        PRE = soup.findAll("pre")
+        L = len(PRE)
+        inde = 1
+        skipped = 0
+        for i in range(L):
+            classAttr = PRE[i].parent.get("class")
+            if classAttr is None or "note" in classAttr:
+                skipped += 1
+                continue
+            elif i % 2 == skipped % 2:
+                In = str(PRE[i])
+                In = In.replace("<pre>", "").replace("</pre>", "")
+                In = In.replace("&gt;", ">")
+                In = In.replace("&lt;", "<")
+                In = In.replace("&quot;", '"')
+                In = In.replace("&amp;", "&")
+                In = In.replace("<br />", "\n")
+                In = In.replace("<br/>", "\n")
+                In = In.replace("</ br>", "\n")
+                In = In.replace("</br>", "\n")
+                In = In.replace("<br>", "\n")
+                In = In.replace("< br>", "\n")
+                In = In.split("\n")
+                filename = f"{PWD}/{str(c)}/{str(inde)}.in"
+                writeFile(In, filename)
+            else:
+                Out = str(PRE[i])
+                Out = Out.replace("<pre>", "").replace("</pre>", "")
+                Out = Out.replace("&gt;", ">")
+                Out = Out.replace("&lt;", "<")
+                Out = Out.replace("&quot;", '"')
+                Out = Out.replace("&amp;", "&")
+                Out = Out.replace("<br />", "\n")
+                Out = Out.replace("<br/>", "\n")
+                Out = Out.replace("</ br>", "\n")
+                Out = Out.replace("<br>", "\n")
+                Out = Out.split("\n")
+                filename = f"{PWD}/{str(c)}/{str(inde)}.out"
+                writeFile(Out, filename)
+                inde += 1
+        print(
+            (Fore.WHITE + "parsing " + str(c))
+            + (Fore.GREEN + "  [Success]  ")
+            + (Fore.WHITE + "")
+        )
+
 
     def parse(self):
-        code = int(input("Contest code XXX: "))
+        self.code = int(input("Contest code XXX: "))
 
-        http = urllib3.PoolManager()
-
-        url = codeForcesURL + str(code)
+        url = codeForcesURL + str(self.code)
         http.request("Get", url)
         page = http.request("Get", url)
         soup = BeautifulSoup(page.data, features="lxml")
         sz = soup.findAll(title="Submit")
 
-        url2 = "/problem/"
         ch = []
-        chtest = []
         for p in soup.findAll("td", attrs={"class": "id"}):
             for s in p.find("a").stripped_strings:
                 ch.append(s)
-                chtest.append(0)
 
-        jj = 0
         print("   Connected!\n")
-        while jj < len(sz):
-            url = f"{codeForcesURL}{str(code)}{url2}{str(ch[jj])}"
-            page = http.request("Get", url)
-            soup = BeautifulSoup(page.data, features="lxml")
-            PRE = soup.findAll("pre")
-            L = len(PRE)
-            inde = 1
-            for i in range(L):
-                classAttr = PRE[i].parent.get("class")
-                if classAttr is None or "note" in classAttr:
-                    break
-                elif i % 2 == 0:
-                    In = str(PRE[i])
-                    In = In.replace("<pre>", "").replace("</pre>", "")
-                    In = In.replace("&gt;", ">")
-                    In = In.replace("&lt;", "<")
-                    In = In.replace("&quot;", '"')
-                    In = In.replace("&amp;", "&")
-                    In = In.replace("<br />", "\n")
-                    In = In.replace("<br/>", "\n")
-                    In = In.replace("</ br>", "\n")
-                    In = In.replace("</br>", "\n")
-                    In = In.replace("<br>", "\n")
-                    In = In.replace("< br>", "\n")
-                    In = In.split("\n")
-                    filename = f"{PWD}/{str(ch[jj])}/{str(inde)}.in"
-                    writeFile(In, filename)
-                else:
-                    Out = str(PRE[i])
-                    Out = Out.replace("<pre>", "").replace("</pre>", "")
-                    Out = Out.replace("&gt;", ">")
-                    Out = Out.replace("&lt;", "<")
-                    Out = Out.replace("&quot;", '"')
-                    Out = Out.replace("&amp;", "&")
-                    Out = Out.replace("<br />", "\n")
-                    Out = Out.replace("<br/>", "\n")
-                    Out = Out.replace("</ br>", "\n")
-                    Out = Out.replace("<br>", "\n")
-                    Out = Out.split("\n")
-                    filename = f"{PWD}/{str(ch[jj])}/{str(inde)}.out"
-                    writeFile(Out, filename)
-                    inde += 1
-                    chtest[jj] += 1
+        with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+            future_to_url = {executor.submit(self.parseProblem, c) for c in ch}
 
-            print(
-                (Fore.WHITE + "parsing " + str(ch[jj]))
-                + (Fore.GREEN + "  [Success]  ")
-                + (Fore.WHITE + "")
-            )
-            jj = jj + 1
-
-        f = open(f'{PWD}/random.txt', "w")
-        f.write(str(code) + "\n")
-        jj = 0
-        while jj < len(sz):
-            f.write(str(ch[jj]) + " " + str(chtest[jj]) + "\n")
-            jj = jj + 1
-        f.close()
-
+         
 
 website = "codeforces"
 parser = ParserFactory.get(website)
