444c239f3f2c53041caf309e4e08e6e21c648c2a
sawamotokai <kaisawamoto@gmail.com>
2022-09-06 23:05:49 -0700

    fix: path for ubuntu

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 pip3 install -r requirements.txt
 sudo cp src/cprun /usr/local/bin/cprun
 sudo cp src/cpget /usr/local/bin/cpget
-mkdir ~/.sample-test-runner
-sudo cp src/parse.py ~/.sample-test-runner/parse.py
+mkdir /usr/local/.sample-test-runner
+sudo cp src/parse.py /usr/local/.sample-test-runner/parse.py
 sudo chmod 755 /usr/local/bin/cprun
 sudo chmod 755 /usr/local/bin/cpget
-sudo chmod 755 ~/.sample-test-runner/parse.py
+sudo chmod 755 /usr/local/.sample-test-runner/parse.py
 BLUE='\033[0;34m'
 NC='\033[0m'
 echo "alias cpget=\". cpget\"" >> ~/.bashrc
